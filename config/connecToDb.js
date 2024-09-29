e5eee2712da9a495f6438d98cc8f815857673d45
mkmeden <midhupayathuparambil@gmail.com>
2024-09-29 14:50:44 +0530

    asddd

--- config/connecToDb.js
+++ config/connecToDb.js
@@ -1,7 +1,6 @@
 
 const mongoose = require("mongoose")
-if(process.env.NODE_ENV!='production')
-    require("dotenv").config()
+
 
 const connecToDb = async () =>{
 
--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 //dependencies
 const express = require("express");
 const connecToDb = require("./config/connecToDb");
-const Note = require("./models/note");
-const {
-  createNote,
-  fetchNote,
-  fetchNotes,
-  updateNote,
-  deleteNote,
-} = require("./controllers/notesControllers");
 const cors = require("cors");
-const {
-  signup,
-  login,
-  logout,
-  checkAuth,
-} = require("./controllers/usersController");
 const cookieParser = require("cookie-parser");
+const {
+  createNote, fetchNote, fetchNotes, updateNote, deleteNote
+} = require("./controllers/notesControllers");
+const {
+  signup, login, logout, checkAuth
+} = require("./controllers/usersController");
 const requireAuth = require("./middleware/requireAuth");
 
 if (process.env.NODE_ENV != "production") require("dotenv").config();
@@ -25,30 +17,35 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production"?"https://notepro-eight.vercel.app": true,
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    origin: process.env.NODE_ENV === "production" ? "https://notepro-eight.vercel.app" : true,
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    credentials: true, // Allow credentials (cookies)
   })
 );
+app.options("*", cors()); // Handle preflight requests
+
 app.use(cookieParser());
 
+// Connect to DB
 connecToDb();
 
-//routes
-app.get('/' , (req,res)=>{
-  res.send("Hello world")
-})
+// Routes
+app.get('/', (req, res) => {
+  res.send("Hello world");
+});
 
 app.post("/signup", signup);
 app.post("/login", login);
 app.get("/logout", logout);
 app.get("/check-auth", requireAuth, checkAuth);
 
-app.post("/notes",requireAuth, createNote);
-app.get("/notes", requireAuth,fetchNotes);
-app.get("/notes/:id", requireAuth,fetchNote);
-app.put("/notes/:id",requireAuth, updateNote);
-app.delete("/notes/:id", requireAuth,deleteNote);
+app.post("/notes", requireAuth, createNote);
+app.get("/notes", requireAuth, fetchNotes);
+app.get("/notes/:id", requireAuth, fetchNote);
+app.put("/notes/:id", requireAuth, updateNote);
+app.delete("/notes/:id", requireAuth, deleteNote);
 
-//Start our server
-app.listen(process.env.PORT);
+// Start our server
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
